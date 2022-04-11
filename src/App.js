87d92d38fd3a9d6107d6cf367fffcb6cf9f04a01
Sharique Anwer <90417256+shariqAnwer@users.noreply.github.com>
2022-04-11 12:40:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React from "react";
+import Result from "./Result";
+import "./styles.css";
+
+export default function App() {
+  const [score, setScore] = React.useState(76);
+  const [wicket, setWicket] = React.useState(2);
+  const [ball, setBall] = React.useState(50);
+
+  const AddScore = (value) => {
+    setScore(score + value);
+  };
+
+  const AddWicket = (value) => {
+    setWicket(wicket + value);
+  };
+
+  const AddBall = (value) => {
+    setBall(ball + value);
+  };
+
+  const stop = () => {
+    var btn = document.querySelector(".addWicket");
+    if (wicket > 12) {
+      disabled = true;
+    }
+  };
+
+  return (
+    <div className="App">
+      <h3>India:</h3>
+      <div className="banner">
+        <div>
+          Score:{""}
+          <h1 className="scoreCount">
+            {
+              // show "score" here
+              score
+            }
+          </h1>
+        </div>
+        <div>
+          Wicket:{""}
+          <h1 className="wicketCount">
+            {
+              // show wicket here
+              wicket
+            }
+          </h1>
+        </div>
+
+        <div>
+          Over:{}
+          <h1 className="overCount">
+            {
+              Math.round(ball / 6) + "." + (ball % 6)
+
+              // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball
+              // if 1 more ball is thrown then over is now 5.0
+              // you have to write logic to form this string from current ball number.
+            }
+          </h1>
+        </div>
+      </div>
+
+      <div className="addScore">
+        Add Score
+        {/* these buttons should add the respective amount in the score */}
+        <button className="addScore1" onClick={() => AddScore(1)}>
+          Add 1
+        </button>
+        <button className="addScore4" onClick={() => AddScore(4)}>
+          Add 4
+        </button>
+        <button className="addScore6" onClick={() => AddScore(6)}>
+          Add 6
+        </button>
+      </div>
+
+      <div className="addWicket">
+        Add Wicket
+        {/* Increase the count of wicket */}
+        <button onClick={() => AddWicket(1)}>Add 1 wicket</button>
+      </div>
+
+      <div className="addBall">
+        Add ball
+        {/* Increase the total number of balls thrown here. */}
+        <button onClick={() => AddBall(1)}>Add 1</button>
+      </div>
+      <Result />
+    </div>
+  );
+}
